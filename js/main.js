05ac03ee91e5bd52a2846648e8f45b68d230d32e
jondmulcahy <mr.mulcahy@outlook.com>
2024-03-01 22:41:54 -0700

    Created getComputerChoice function.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+    const choice = ["rock", "paper", "scissors"];
+    let result = choice[Math.floor(Math.random() * 3)];
+    console.log("Computer chooses " + result);
+    return result;
+  }
