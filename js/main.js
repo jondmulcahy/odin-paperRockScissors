6f38691b1339cfa28c7d4c6c7ddbe9f1ba1243b1
jondmulcahy <mr.mulcahy@outlook.com>
2024-03-01 22:45:14 -0700

    Created playRound function.

--- js/main.js
+++ js/main.js
@@ -7,3 +7,23 @@ function getComputerChoice() {
     console.log("Computer chooses " + result);
     return result;
   }
+
+  function playRound() {
+    if (playerSelection == computerSelection) {
+      console.log("It is a tie!");
+      return;
+    };
+    if (playerSelection == "rock" && computerSelection == "paper") {
+      console.log("Computer wins");
+      return;
+    }
+    if (playerSelection == "paper" && computerSelection == "scissors") {
+      console.log("Computer wins");
+      return;
+    };
+    if (playerSelection == "scissors" && computerSelection == "rock") {
+      console.log("Computer wins");
+      return;
+    }
+    console.log("You Win!");
+  }
